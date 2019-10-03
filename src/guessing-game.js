e0b9232127037cfdd9bf848ec1c648c95b1fafef
Zhdanovich Katsiaryna <zhdanovichkat@gmail.com>
2019-10-03 09:24:07 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
+    constructor() {
+        this.minimum = null;
+        this.maximum = null;
     }
-
-    guess() {
-
+//this method accepts min and max value of range of number to guess
+    setRange(min, max) { 
+        this.minimum = min;
+        this.maximum = max;
     }
-
-    lower() {
-
+//this method returns solution candidate (you make an assumption based on range and previous assumptions)
+    guess() { 
+        return Math.ceil((this.maximum+this.minimum)/2);        
     }
-
+//this method is called if prev call of guess() returned number which is greater than answer
+    lower() { 
+        this.maximum = this.guess();
+    }
+//this method is called if prev call of guess() returned number which is lower than answer
     greater() {
-
+        this.minimum = this.guess();
     }
 }
 
